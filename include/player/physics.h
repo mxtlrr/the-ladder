1004fb9e643c279bc82946b3917d3be8ee1ffc17
mxtlrr <ruson0914@gmail.com>
2023-04-14 19:58:34 -0500

    begin work on the ''''physics'''' engine

---
+++ include/player/physics.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+// The raylib camera uses these values to move the camera up or down.
+// -1.0f -- down
+// 0.0f  -- neither
+// 1.0f  -- up
+#define YES -1.0f
+#define NO  0.0f
+
+/* Should the player move down, depending on it's current Y position? */
+float move_down__(float y_pos);
