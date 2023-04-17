c6a16a04bd5b0a654ed4013efa90d14fcbdae5d0
mxtlrr <ruson0914@gmail.com>
2023-04-16 20:46:42 -0500

    i think i'm gonna cry this is harder than i thought

--- src/player/physics.c
+++ src/player/physics.c
@@ -4,6 +4,12 @@
 
 #define DIFFERENCE 2.0f /* magic! :) */
 
+#ifdef GAP
+  #define FIXED_GAP 0.5f
+#else
+  #define GAP 0.5f
+#endif
+
 /* this code is about to give me a mental breakdown
  * maybe future me can figure it out.
  
@@ -13,18 +19,58 @@ int done = 0x0000;
 float z_start = -0.487900f;
 float z_end   =  0.511940f;
 
+/* Find the start Z position of the stair */
+float find_starting_stair(int stair_n, float stair0_pos,
+                          float width){
+  /* no idea why this works 
+    but it odes so i can't complain */
+  const float MAGIC_START = 0.49984;
+  if(stair_n > 2){
+    // stair0 pos is the original starting position of the first stair
+    //
+    // basically:
+    // ((stair0+width)-0.49984)+0.5 gives stair 1's initial position
+    // if we use stair 1's position in place of stair 0, we should
+    // theoretically get stair 2's position.
+    float stair_0 =((stair0_pos+width)-MAGIC_START)+FIXED_GAP;
+    float new = 0.0f;
+    float stair__n = stair_0;
+    for(int i = 0; i != stair_n+1; i++){
+      stair__n = ((stair__n+width)-MAGIC_START)+FIXED_GAP;
+      new = stair__n;
+      /* new = stair_n */
+
+    }
+    return new-1.0f;
+  } else {
+    // i need to come up with something new
+  }
+}
+
 
 float move_down__(float y_pos, player_t player, stair_t starting,
                   Camera camera){
   float len = z_end-z_start; // 0.99984
+  float starting_ = z_start+0.487900; // where stair 0 starts
+
   stair_t* stairs = get_stairs_z();
 
-  /* this is a horrible way to do it */
-  // printf("stair 1 should be at (z=%.6f), width in z is %.6f.\n",
-  //        (0.0f+len-1.48774)*-4.074, len);
-  // printf("reality: (z=%.6f), width in z is %.6f.\n",
-  //        stairs[2].dimensions.y+0.4879, stairs[2].width-0.00016);
+  // now we can calculate where step 1 can be
+  /* Add the starting position of stair 0 to it's width,
+     Followed by the gap between stairs. */
+  printf("%.6f\n", ((starting_+len)-0.49984)+FIXED_GAP);
+  printf("test: %.6f\n", find_starting_stair(0, (z_start+0.487900), len));
+  
+  float stair0pos = ((starting_+len)-0.49984)+FIXED_GAP;
+  float stair1pos = ((stair0pos+len)-0.49984)+FIXED_GAP;
+  printf("%.6f\n", stair1pos);
 
+  // printf("stair 0 starts at %.6f (reality=%.6f)\n",
+  //       starting_, stairs[0].dimensions.y/*-0.2*/);
+  // printf("Place where step 1 should be: %.6f (reality is %.6f)\n",
+  //       step_1, stairs[1].dimensions.y);
+
+ 
   if(y_pos == starting.y_pos+2 && /* required! */
     (player.position.x >= starting.dimensions.x &&
      player.position.x < (starting.dimensions.x+starting.width))
